96f97a156bdf6d955e724cd1fee7c485384bdda4
bloodmask <kiremate@hotmail.com>
2023-03-09 17:52:51 +0100

    Implemented movement events in Camera

--- source/Camera.h
+++ source/Camera.h
@@ -1,67 +1,71 @@
 #pragma once
-
-#include <SFML/System/Vector3.hpp>
 #include "math.hpp"
+#include <SFML/Window/Event.hpp>
 
 using namespace example;
 
-
 struct Transform {
     Vector3f position;
-    Vector3f rotation;
+    Quaternion rotation;
     Vector3f scale;
 
     Matrix44 get_transform_matrix() {
-        Matrix44 translation_matrix = translate(Matrix44(1.0f), position);
-        Matrix44 rotation_matrix = rotation_z() * rotation_y() * rotation_x();
         Matrix44 scale_matrix = glm::scale(Matrix44(1.0f), scale);
-
+        Matrix44 rotation_matrix = glm::mat4_cast(rotation);
+        Matrix44 translation_matrix = glm::translate(Matrix44(1.0f), position);
         return translation_matrix * rotation_matrix * scale_matrix;
     }
 
-    // Add a function to handle translation
-    void translate_local(Vector3f delta_position) {
-        position += rotation_matrix() * delta_position;
+    void move(const Vector3f& offset) {
+        position += offset;
     }
 
-    // Add functions to handle rotation around each axis
-    Matrix44 rotation_x() {
-        return rotate_around_x(Matrix44(1.0f), rotation.x);
+    void rotate(const Quaternion& q) {
+        rotation = glm::normalize(rotation * q);
     }
 
-    Matrix44 rotation_y() {
-        return rotate_around_y(Matrix44(1.0f), rotation.y);
+    void set_rotation(const Quaternion& q) {
+        rotation = q;
     }
 
-    Matrix44 rotation_z() {
-        return rotate_around_z(Matrix44(1.0f), rotation.z);
+    void scale_up(const Vector3f& factor) {
+        scale *= factor;
     }
 
-    // Helper function to get the rotation matrix
-    Matrix44 rotation_matrix() {
-        return rotation_z() * rotation_y() * rotation_x();
+    void scale_down(const Vector3f& factor) {
+        scale /= factor;
     }
 
-    // Add functions to handle scaling
-    void scale_x(float factor) {
-        scale.x *= factor;
+    void translate_local(const Vector3f& offset) {
+        // Get the rotation matrix from the quaternion
+        Matrix44 rotation_matrix = glm::mat4_cast(rotation);
+
+        // Transform the offset vector by the rotation matrix
+        Vector4f transformed_offset = glm::vec4(offset, 0.0f) * rotation_matrix;
+
+        // Add the transformed offset to the position
+        position += Vector3f(transformed_offset);
     }
 
-    void scale_y(float factor) {
-        scale.y *= factor;
-    }
-
-    void scale_z(float factor) {
-        scale.z *= factor;
+    void rotate(float pitch, float yaw) {
+        // Update the rotation quaternion with the new pitch and yaw angles
+        Quaternion q_yaw = glm::angleAxis(glm::radians(yaw), Vector3f(0, 1, 0));
+        Quaternion q_pitch = glm::angleAxis(glm::radians(pitch), Vector3f(1, 0, 0));
+        rotation = glm::normalize(rotation * q_yaw * q_pitch);
     }
 };
 
 class Camera {
-	
-	Matrix44 get_inverse_transform_matrix() {
-		return inverse(transform.get_transform_matrix());
-	}
-
+    
+public:
+    Matrix44 get_view_matrix();
+    Matrix44 get_inverse_transform_matrix();
+    void handle_event(const sf::Event& event, float delta_time);
+    Camera() = default;
+    Camera(float fov, float near, float far, Vector3f position, Vector3f target, Vector3f up) :
+        fov(fov), near(near), far(far), transform({ position, Vector3f(0.0f), Vector3f(1.0f) }), view_direction(normalize(target - position)), up_direction(normalize(up))
+    {
+    }
 private:
     
     float fov;
